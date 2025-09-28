e3b3c09814239adb0a6f2057ff35d9b1711545cc
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2025-09-28 22:27:17 +0900

    chore: add ToString annotation

--- src/main/java/com/imfine/ngs/order/entity/Order.java
+++ src/main/java/com/imfine/ngs/order/entity/Order.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,6 +14,7 @@ import java.util.UUID;
 @Entity
 @Table(name = "orders")
 @Getter
+@ToString
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Order extends BaseTimeEntity {
 
